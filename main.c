3004a6d5fcb013227411723060bb815fad0bab3b
emil.denelin@hotmail.com <emil.denelin@hotmail.com>
2025-05-23 16:32:24 +0200

    Switch test

--- main.c
+++ main.c
@@ -8,8 +8,47 @@ int main()
 
    
     // Your code goes here
+    int roll[100];
+    int results[] = {0, 0, 0, 0, 0, 0};
+    int sum = 0;
 
-    
+    // Randomize and store numbers
+    for (int i = 0; i < 100; i++)
+    {
+        roll[i] = (rand() % 6) + 1;
+        switch (roll[i])
+        {
+        case 1:
+            results[0]++;
+            break;
+        case 2:
+            results[1]++;
+            break;
+        case 3:
+            results[2]++;
+            break;
+        case 4:
+            results[3]++;
+            break;
+        case 5:
+            results[4]++;
+            break;
+        case 6:
+            results[5]++;
+            break;
+        
+        default:
+            break;
+        }
+        sum = sum + roll[i];
+    }
+
+    // Print results
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d\n", results[i]);
+    }
+    printf("%d\n", sum);
 
     return 0;
 }
